c1999ea21379336e460518785cd8c62c28b721f4
Nikita-staric <moses.nikitos@gmail.com>
2021-09-05 19:32:50 +0300

    first commit

---
+++ оружие/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace оружие
+{
+    class Program
+    {
+        private bool gg;
+        public void Main()
+        {
+
+            Console.WriteLine("Заряжаю");
+            gg = true;
+            Console.WriteLine("Оружие заряжено");
+        }
+        public void AA()
+        {
+            if(!gg)
+            {
+                Console.WriteLine("Не заряжено");
+                Main();
+            }
+            Console.WriteLine("\t \n \a Пиу пиу ");
+            gg = false;
+
+        }
+    }
+
+    class KK
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            Program dd = new Program();
+            dd.AA();
+            Console.ReadKey();
+
+        }
+    }
+    }
+
+
